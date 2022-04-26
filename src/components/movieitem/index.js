be5c2a9f14022408ec9f1c28c5cad8191ae6ddbf
Andres Saemundsson <andresthor@gmail.com>
2022-04-26 16:04:39 +0200

    feat: show movie info in MovieItem

--- src/components/movieitem/index.js
+++ src/components/movieitem/index.js
@@ -1,35 +1,91 @@
-import React from "react";
+import React from 'react';
 import styled from 'styled-components';
 
-export default function MovieItem ({ movie, genres }) {
+import * as colors from '../../colors';
+import { getImageUrl } from '../../fetcher';
+
+export default function MovieItem({ movie, genres }) {
   return (
-    // TODO: Complete the MovieItem component
     <MovieItemWrapper>
       <LeftCont>
+        <img src={getImageUrl(movie.poster_path)} alt={movie.title} />
       </LeftCont>
       <RightCont>
-        <Title>{movie.title}</Title>
+        <Header>
+          <Title>{movie.title}</Title>
+          <Rating>{movie.vote_average.toFixed(1)}</Rating>
+        </Header>
+        <Genres>action | comedy | adventure</Genres>
+        <Overview>{movie.overview}</Overview>
+        <Date>
+          <span>{movie.release_date}</span>
+        </Date>
       </RightCont>
     </MovieItemWrapper>
-  )
+  );
 }
 
 const MovieItemWrapper = styled.div`
+  display: flex;
+  flex-flow: row nowrap;
   position: relative;
   background-color: white;
   border-radius: 5px;
-  padding: 20px;
   margin: 15px 0;
-`
+  padding: 20px;
+`;
 
 const LeftCont = styled.div`
-  display: inline-block;
-`
+  display: flex;
+
+  > img {
+    width: 130px;
+    height: fit-content;
+  }
+`;
 
 const RightCont = styled.div`
-  display: inline-block;
-`
+  display: flex;
+  flex-direction: column;
+  margin-left: 20px;
+`;
+
+const Header = styled.div`
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: space-between;
+  align-items: center;
+`;
 
 const Title = styled.h2`
-  font-size: 1.4;
+  margin: 0;
+  font-size: 1.4em;
+`;
+
+const Rating = styled.div`
+  background-color: ${colors.primaryColor};
+  color: white;
+  height: fit-content;
+  padding: 0.25em;
+  border-radius: 20%;
+`;
+
+const Genres = styled.p`
+  margin: 0;
+  font-size: 0.7em;
+  color: ${colors.primaryColor};
+`;
+
+const Overview = styled.p`
+  font-size: 0.85em;
+`;
+
+const Date = styled.p`
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
+  justify-content: flex-end;
+  margin: 0;
+  font-size: 0.7em;
+  color: ${colors.primaryColor};
 `;
