79afc458ba8a4eea796186297c9853da0c107d58
Andres Saemundsson <andresthor@gmail.com>
2022-04-26 16:33:12 +0200

    feat: add genre api call

--- src/fetcher.js
+++ src/fetcher.js
@@ -8,7 +8,7 @@ const API_URL = 'https://api.themoviedb.org/3';
 const IMAGE_URL = 'https://image.tmdb.org/t/p/original';
 
 const popularMovies = `${API_URL}/movie/popular`;
-// const movieGenres = `${API_URL}/genre/movie/list`;
+const movieGenres = `${API_URL}/genre/movie/list`;
 // const searchMovies = `${API_URL}/search/movie`;
 
 export const getImageUrl = (id) => `${IMAGE_URL}/${id}`;
@@ -16,6 +16,15 @@ export const getImageUrl = (id) => `${IMAGE_URL}/${id}`;
 export async function getPopularMovies() {
   try {
     const response = await axios.get(popularMovies, { params });
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function getMovieGenres() {
+  try {
+    const response = await axios.get(movieGenres, { params });
     return response.data;
   } catch (error) {
     console.error(error);
